0b8ec851cc2d7ebfe01adc700c441e98a5b3dfee
JClique <j.leadbetter@me.com>
2020-08-06 11:11:04 +0100

    Fixed animation and sizing of bar charts.

--- src/components/graph/BarChart.vue
+++ src/components/graph/BarChart.vue
@@ -3,17 +3,17 @@
     <svg :width="chartWidth + 20" :height="chartHeight + 20">
 
       <g v-if="this.chartLines">
-        <line :x1="0" :x2="chartWidth + 10" :y1="chartHeight + 10" :y2="chartHeight + 10"></line>
-        <line :x1="1" :x2="1" :y1="0" :y2="chartHeight + 10"></line>
+        <line :x1="10" :x2="chartWidth + 10" :y1="chartHeight + 10" :y2="chartHeight + 10"></line>
+        <line :x1="11" :x2="11" :y1="0" :y2="chartHeight + 10"></line>
 
         <line class="marker" v-for="i in 10" :key="i"
           :x1="(dataSteps * i) + 9" :x2="(dataSteps * i) + 9" :y1="0" :y2="chartHeight + 9"></line>
       </g>
 
-      <g v-for="(value, i) in chartData" :key="i"
+      <g v-for="(value, i) in animatedChartData" :key="i"
         :transform="`translate(0, ${i * 40})`">
         <rect height="30" :width="barWidth(value)" x="10"></rect>
-        <text y="20" :x="barWidth(value)">{{ value }}</text>
+        <text y="20" :x="barWidth(value)">{{ value | round }}</text>
       </g>
     </svg>
   </div>
@@ -23,9 +23,43 @@
 export default {
   name: "BarChart",
   props: ["chartData", "chartWidth", "chartHeight", "chartLines"],
+  data() {
+    return {
+      animatedChartData: [0,0,0,0,0,0,0,0,0,0,0,0]
+    }
+  },
   methods: {
     barWidth(value) {
       return this.chartWidth / this.dataMax * value;
+    },
+    animate() {
+      const fromValues = this.animatedChartData;
+      const toValues = this.chartData;
+
+      const duration = 600;
+      const start = Date.now();
+
+      const frame = () => {
+        const elapsed = Date.now() - start;
+        const percent = elapsed / duration;
+
+        if (percent >= 1) {
+          this.animatedChartData = this.chartData;
+        } else {
+          this.animatedChartData = toValues.map((toVal, i) => {
+            const fromVal = fromValues[i];
+            return fromVal + (toVal - fromVal) * percent;
+          });
+          requestAnimationFrame(frame);
+        }
+      };
+
+      requestAnimationFrame(frame);
+    },
+  },
+  watch: {
+    chartData() {
+      this.animate();
     },
   },
   computed: {
@@ -35,16 +69,20 @@ export default {
     dataSteps() {
       return this.chartWidth / 10;
     }
-  }
+  },
+  filters: {
+  round(number) {
+    return Math.round(number);
+  },
+},
 }
 </script>
 
 <style scoped>
 
   .bar {
-    padding: 1rem;
-    padding-bottom: 0;
-    transition: .4s;
+    padding: 0;
+    padding-top: 1rem;
   }
 
   line {
@@ -73,7 +111,6 @@ export default {
 
   rect {
     fill: var(--pink);
-    transition: .6s;
     transition-property: width;
   }
 
